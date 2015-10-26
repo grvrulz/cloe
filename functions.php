68617b61b0f96e161be3c91de6db370dbfb0f941
Gaurav Pareek <me@grv.rocks>
2015-10-26 14:58:08 +0530

    New fonts

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ define( 'CHILD_THEME_VERSION', '1.0.0' );
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
 
-	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Playfair+Display|Muli', array(), CHILD_THEME_VERSION );
 
 }
 
