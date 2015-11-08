10fbfb48a3fd75c8302ce414cbb8b670aea7e070
Gaurav Pareek <me@grv.rocks>
2015-11-09 00:46:44 +0530

    Enqueue dashicons

--- functions.php
+++ functions.php
@@ -15,6 +15,7 @@ add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
 
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Playfair+Display|Muli', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'dashicons' );
 
 }
 
